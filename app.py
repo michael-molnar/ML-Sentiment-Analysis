9772b660683ce1046015cb86a0f1414abd266f42
michael-molnar <71237906+michael-molnar@users.noreply.github.com>
2020-12-12 00:36:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -97,31 +97,6 @@ def process_input(text):
     text = remove_stopwords(text)
     return text
 
-"""
-I want to use the model to generate few words and phrases.
-To do this I use the coefficients of hte model and the terms of the vectorizer.
-"""
-# The coefficients for the negative class
-"""negative_coef = {
-    word: coef for word, coef in zip(
-        vectorizer.get_feature_names(), model.coef_[0])
-    }
-
-# The coefficients for the neutral class
-neutral_coef = {
-    word: coef for word, coef in zip(
-        vectorizer.get_feature_names(), model.coef_[1])
-    }
-
-# The coefficients for the positive class
-positive_coef = {
-    word: coef for word, coef in zip(
-        vectorizer.get_feature_names(), model.coef_[2])
-    }
-
-"""
-
-
 @app.route('/')
 def home():
 	return render_template('index.html')
@@ -146,84 +121,6 @@ def predict():
     pred_text4 = 'Probability for Positive: {}%'.format(pos_prop)
 
     return render_template('index.html', your_text = input_text, prediction_text = pred_text1, neg_text = pred_text2, neu_text = pred_text3, pos_text = pred_text4)
-"""
-@app.route('/predict_analyze', methods=['POST', 'GET'])
-def predict_analyze():
-    input_text = request.form['your_text']
-    processed_text = [process_input(input_text)]
-    vec_text = vectorizer.transform(processed_text)
 
-    prediction = model.predict(vec_text)
-    confidence = model.predict_proba(vec_text)
-    output = prediction[0].upper()
-    neg_prop = np.around(confidence[0][0]*100, 2)
-    neu_prop = np.around(confidence[0][1]*100, 2)
-    pos_prop = np.around(confidence[0][2]*100, 2)
-
-    pred_text1 = 'The Predicted Sentiment is: {}'.format(output)
-    pred_text2 = 'Probability for Negative: {}%'.format(neg_prop)
-    pred_text3 = 'Probability for Neutral: {}%'.format(neu_prop)
-    pred_text4 = 'Probability for Positive: {}%'.format(pos_prop)
-
-    # Get the features of the input text and store the English words or phrases
-    phrases = []
-    for item in vec_text.indices:
-        phrases.append(vectorizer.get_feature_names()[item])
-    
-    #Create a dictionary of the model's coefficients for these features 
-    importances = dict()
-    for phrase in phrases:
-        if prediction == 'negative':
-            importances[phrase] = negative_coef.get(phrase)
-        elif prediction == 'neutral':
-            importances[phrase] = neutral_coef.get(phrase)
-        elif prediction == 'positive':
-            importances[phrase] = positive_coef.get(phrase)
-            
-    # Sort this dictionary according to its values
-    importances = sorted(importances.items(), key=lambda x: x[1], reverse=True)
-    importances = dict(importances[:20])
-    pred_text1 = 'The Predicted Sentiment is: {}'.format(output)
-    pred_text2 = 'Probability for Negative: {}%'.format(neg_prop)
-    pred_text3 = 'Probability for Neutral: {}%'.format(neu_prop)
-    pred_text4 = 'Probability for Positive: {}%'.format(pos_prop)
-       
-    pred_text5 = 'Top Ten Phrases'"""
-    #cloud = WordCloud(background_color='black').generate_from_frequencies(importances)
-    #plt.figure(figsize=(6, 6), facecolor=None)
-    #plt.imshow(cloud, interpolation="bilinear")
-    #plt.axis("off")
-    #plt.tight_layout(pad=0)
-
-    #new_graph_name = "graph" + str(time.time()) + ".png"
-
-    #for filename in os.listdir('static/images'):
-     #   if filename.startswith('graph_'):  # not to remove other images
-      #      os.remove('static/' + filename)
-
-    #plt.savefig('static/images/' + new_graph_name, bbox_inches='tight')
-
-
-    #plt.savefig('static\images\word_cloud.png', bbox_inches="tight")
-    """
-                pred_text6 = importances[0][0]
-                pred_text7 = importances[1][0]
-                pred_text8 = importances[2][0]
-                pred_text9 = importances[3][0]
-                pred_text10 = importances[4][0]
-                pred_text11 = importances[5][0]
-                pred_text12 = importances[6][0]
-                pred_text13 = importances[7][0]
-                pred_text14 = importances[8][0]
-                pred_text15 = importances[9][0]
-                
-            
-            
-            
-                return render_template('index.html', prediction_text = pred_text1, your_text = input_text, neg_text = pred_text2, neu_text = pred_text3, pos_text = pred_text4,
-                    analysis_text = pred_text5, analysis_text2 = pred_text6, analysis_text3 = pred_text7, analysis_text4 = pred_text8, 
-                    analysis_text5 = pred_text9, analysis_text6 = pred_text10, analysis_text7 = pred_text11, analysis_text8 = pred_text12, analysis_text9 = pred_text13,
-                    analysis_text10 = pred_text14, analysis_text11 = pred_text15)"""
- #url='static/images/'#
 if __name__ == '__main__':
 	app.run(debug=True)
